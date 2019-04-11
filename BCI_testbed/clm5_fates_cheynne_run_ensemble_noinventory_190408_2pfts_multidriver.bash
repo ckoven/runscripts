f4f0289d029ad5306ce5a9aafef23ed82fc5141f
ckoven <ckoven@gmail.com>
2019-04-11 10:55:35 -0600

    updated 1 PFT case, added 2-PFT and single-instance hydro cases

--- BCI_testbed/clm5_fates_cheynne_run_ensemble_noinventory_190408_2pfts_multidriver.bash
+++ BCI_testbed/clm5_fates_cheynne_run_ensemble_noinventory_190408_2pfts_multidriver.bash
@@ -6,9 +6,9 @@ GITHASH1=`git log -n 1 --format=%h`
 cd src/fates
 GITHASH2=`git log -n 1 --format=%h`
 
-SETUP_CASE=fates_clm5_fullmodel_bci_parameter_ensemble_1pft_190329_multiinst_576inst_cont_moresizebins
+SETUP_CASE=fates_clm5_fullmodel_bci_parameter_ensemble_2pfts_190408_multiinst_36inst_noinv
 
-if [ "${SETUP_CASE}" == "fates_clm5_fullmodel_bci_parameter_ensemble_1pft_190329_multiinst_576inst_cont_moresizebins" ]; then
+if [ "${SETUP_CASE}" == "fates_clm5_fullmodel_bci_parameter_ensemble_2pfts_190408_multiinst_36inst_noinv" ]; then
     CASE_NAME=${SETUP_CASE}_${GITHASH1}_${GITHASH2}
     basedir=$HOME/ctsm/cime/scripts
     export SITE_NAME=bci_0.1x0.1_v4.0i                         # Name of folder with site data
@@ -21,7 +21,7 @@ if [ "${SETUP_CASE}" == "fates_clm5_fullmodel_bci_parameter_ensemble_1pft_190329
     cd $basedir
     export RES=CLM_USRDAT
     project=P93300041
-    ninst=576
+    ninst=36
     ./create_newcase -case ${CASE_NAME} -res ${RES} -compset I2000Clm50FatesGs -mach cheyenne -project $project --run-unsupported --ninst=$ninst --multi-driver
     cd ${CASE_NAME}
     export DIN_LOC_ROOT_FORCE=${SITE_BASE_DIR}
@@ -29,10 +29,11 @@ if [ "${SETUP_CASE}" == "fates_clm5_fullmodel_bci_parameter_ensemble_1pft_190329
     export CLM_DOMAIN_DIR=${SITE_BASE_DIR}/${SITE_NAME}
 
     ./xmlchange STOP_OPTION=nyears
-    ./xmlchange STOP_N=50
-    ./xmlchange REST_N=50
+    ./xmlchange STOP_N=1
+    ./xmlchange REST_N=1
     ./xmlchange CONTINUE_RUN=FALSE
     ./xmlchange DEBUG=FALSE
+    ./xmlchange RESUBMIT=0
     
     ./xmlchange DIN_LOC_ROOT=/glade/u/home/charlie/cesm_input_data
 
@@ -59,23 +60,24 @@ if [ "${SETUP_CASE}" == "fates_clm5_fullmodel_bci_parameter_ensemble_1pft_190329
 
     ./xmlchange RUN_STARTDATE=0001-06-01
 
-    ./xmlchange RESUBMIT=1
+    ./xmlchange MAX_TASKS_PER_NODE=30
+    ./xmlchange MAX_MPITASKS_PER_NODE=30
+
 
     for x  in `seq 1 1 $ninst`; do
 	expstr=$(printf %04d $x)
 	echo $expstr
 	cat > user_nl_clm_$expstr <<EOF
 fsurdat = '${CLM_SURFDAT_DIR}/${CLM_USRDAT_SURDAT}'
-fates_paramfile = '/glade/scratch/charlie/parameter_file_sandbox/fates_params_default_106ac7a_mod1PFT_exp1_${expstr}.c190329.moredbhbins.nc'
+fates_paramfile = '/glade/scratch/charlie/parameter_file_sandbox/fates_params_default_106ac7a_mod2PFTs_exp1_${expstr}.c190408.nc'
 use_fates_inventory_init = .false.
-finidat = '/glade/u/home/charlie/restfiles/fates_clm5_fullmodel_bci_parameter_ensemble_1pft_190329_multiinst_576inst_b9c92b7_106ac7a.clm2_${expstr}.r.0201-06-01-00000.nc'
-fates_inventory_ctrl_filename = '${SITE_BASE_DIR}/bci_inv_file_list.txt'
+fates_inventory_ctrl_filename = '${SITE_BASE_DIR}/bci_inv_file_list_2identicalpfts.txt'
 use_fates_ed_st3 = .false.
 hist_empty_htapes = .false.
 hist_mfilt = 60, 10
 hist_nhtfrq = 0, -8760
 hist_fexcl1 = 'AGB_SCLS','BA_SCLS','CANOPY_AREA_BY_AGE','CANOPY_HEIGHT_DIST','CROWNAREA_CAN','DDBH_CANOPY_SCLS','DDBH_UNDERSTORY_SCLS','FATES_c_to_litr_cel_c','FATES_c_to_litr_lab_c','FATES_c_to_litr_lig_c','FUEL_MOISTURE_NFSC','H2OSOI','HR_vr','LAI_BY_AGE','LAI_CANOPY_SCLS','LAI_UNDERSTORY_SCLS','LEAF_HEIGHT_DIST','LITR1C_vr','LITR1N_vr','LITR2C_vr','LITR2N_vr','LITR3C_vr','LITR3N_vr','M1_SCLS','M2_SCLS','M3_SCLS','M4_SCLS','M5_SCLS','M6_SCLS','M7_SCLS','M8_SCLS','MORTALITY','MORTALITY_CANOPY_SCLS','MORTALITY_UNDERSTORY_SCLS','NPLANT_CANOPY_SCLS','NPLANT_SCAG','NPLANT_SCLS','NPLANT_UNDERSTORY_SCLS','O_SCALAR','PATCH_AREA_BY_AGE','PCT_GLC_MEC','PCT_LANDUNIT','PFTbiomass','PFTleafbiomass','PFTnindivs','PFTstorebiomass','RECRUITMENT','SMINN_vr','SMP','SOIL1C_vr','SOIL1N_vr','SOIL2C_vr','SOIL2N_vr','SOIL3C_vr','SOIL3N_vr','SOILICE','SOILLIQ','TLAKE','TSOI','TSOI_ICE','T_SCALAR','W_SCALAR','ZSTAR_BY_AGE'
-hist_fincl2 = 'ZSTAR_BY_AGE','RECRUITMENT','PFTbiomass','PATCH_AREA_BY_AGE','NPLANT_UNDERSTORY_SCLS','NPLANT_SCLS','NPLANT_SCAG','NPLANT_CANOPY_SCLS','MORTALITY_UNDERSTORY_SCLS','MORTALITY_CANOPY_SCLS','MORTALITY','M8_SCLS','M7_SCLS','M6_SCLS','M5_SCLS','M4_SCLS','M3_SCLS','M2_SCLS','M1_SCLS','LEAF_HEIGHT_DIST','LAI_UNDERSTORY_SCLS','LAI_CANOPY_SCLS','LAI_BY_AGE','FUEL_MOISTURE_NFSC','DDBH_UNDERSTORY_SCLS','DDBH_CANOPY_SCLS','CROWNAREA_CAN','CANOPY_HEIGHT_DIST','CANOPY_AREA_BY_AGE','BA_SCLS','AGB_SCLS','GROWTHFLUX_SCPF','GROWTHFLUX_FUSION_SCPF'
+hist_fincl2 = 'ZSTAR_BY_AGE','RECRUITMENT','PFTbiomass','PATCH_AREA_BY_AGE','NPLANT_UNDERSTORY_SCPF','NPLANT_SCPF','NPLANT_SCAG','NPLANT_CANOPY_SCPF','MORTALITY_UNDERSTORY_SCPF','MORTALITY_CANOPY_SCPF','MORTALITY','M8_SCPF','M7_SCPF','M6_SCPF','M5_SCPF','M4_SCPF','M3_SCPF','M2_SCPF','M1_SCPF','LEAF_HEIGHT_DIST','LAI_UNDERSTORY_SCLS','LAI_CANOPY_SCLS','LAI_BY_AGE','FUEL_MOISTURE_NFSC','DDBH_UNDERSTORY_SCPF','DDBH_CANOPY_SCPF','CROWNAREA_CAN','CANOPY_HEIGHT_DIST','CANOPY_AREA_BY_AGE','BA_SCPF','GROWTHFLUX_SCPF','GROWTHFLUX_FUSION_SCPF'
 EOF
 
 
